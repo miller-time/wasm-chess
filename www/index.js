c1b8362e73bc26badd23cf253bc358d37c3fe51f
Russell <miller.time.baby@gmail.com>
2021-02-05 22:45:03 -0800

    add .editorconfig for 2-space css/js indent

--- www/index.js
+++ www/index.js
@@ -4,9 +4,9 @@ const board = Board.new();
 // console.log(board.render());
 
 document.addEventListener('click', (event) => {
-    if (event.target.id) {
-        console.log(event.target.id);
-    } else if (event.target.parentNode.id) {
-        console.log(event.target.parentNode.id);
-    }
+  if (event.target.id) {
+    console.log(event.target.id);
+  } else if (event.target.parentNode.id) {
+    console.log(event.target.parentNode.id);
+  }
 });
