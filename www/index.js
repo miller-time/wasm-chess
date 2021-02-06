4ea5eb540481a9904e415bc48188a24cda7b5b01
Russell <miller.time.baby@gmail.com>
2021-02-05 22:38:17 -0800

    add coordinate id to each square

--- www/index.js
+++ www/index.js
@@ -1,4 +1,12 @@
 import { Board } from "wasm-chess";
 
 const board = Board.new();
-console.log(board.render());
+// console.log(board.render());
+
+document.addEventListener('click', (event) => {
+    if (event.target.id) {
+        console.log(event.target.id);
+    } else if (event.target.parentNode.id) {
+        console.log(event.target.parentNode.id);
+    }
+});
